7b262890de0b1d8c2bb191c47b8d7f96da72384a
U.FRIEDRICH <ugo.friedrich@viveris.fr>
2019-06-14 15:49:23 +0200

    Réorganisation fonctionnement

---
+++ public/js/common.js
@@ -0,0 +1,23 @@
+var WebHelper = new function() {
+
+    this.basepath = '';
+
+    this.init = function init(basepath) {
+        this.basepath = basepath;
+    };
+
+    /**
+     *
+     * @param {string} path
+     * @returns {undefined}
+     */
+    this.getUrl = function getUrl(path) {
+        if (path.charAt(0) === '/') {
+            return this.basepath + path;
+        } else {
+            return document.URL + '/' + path;
+        }
+    };
+};
+
+
--- server.js
+++ server.js
@@ -3,86 +3,51 @@
 
 // init project
 var express = require('express');
-// setup a new database
-var Datastore = require('nedb'), 
-    // Security note: the database is saved to the file `datafile` on the local filesystem. It's deliberately placed in the `.data` directory
-    // which doesn't get copied if someone remixes the project.
-    db = new Datastore({ filename: '.data/datafile', autoload: true });
 var app = express();
 
-// default user list
-var users = [
-      {"firstName":"John", "lastName":"Hancock"},
-      {"firstName":"Liz",  "lastName":"Smith"},
-      {"firstName":"Ahmed","lastName":"Khan"}
-    ];
-
-db.count({}, function (err, count) {
-  console.log("There are " + count + " users in the database");
-  if(err) console.log("There's a problem with the database: ", err);
-  else if(count<=0){ // empty database so needs populating
-    // default users inserted in the database
-    db.insert(users, function (err, usersAdded) {
-      if(err) console.log("There's a problem with the database: ", err);
-      else if(usersAdded) console.log("Default users inserted in the database");
-    });
-  }
-});
+// we've started you off with Express,
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('css'));
-app.use(express.static('script'));
-app.use(express.static('view'));
+app.use(express.static('public'));
+app.use(express.static('public/js/common.js'));
+
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/view/index.html');
+app.get('/*+[^.][^c][^s][^s]', function(request, response) {
+  response.sendFile(__dirname + '/view' + request['url']);
 });
-
-app.get("/users", function (request, response) {
-  var dbUsers=[];
-  db.find({}, function (err, users) { // Find all users in the collection
-    users.forEach(function(user) {
-      dbUsers.push([user.firstName,user.lastName]); // adds their info to the dbUsers value
-    });
-    response.send(dbUsers); // sends dbUsers back to the page
-  });
-});
-
-// creates a new entry in the users collection with the submitted values
-app.post("/users", function (request, response) {
-  db.insert({ firstName: request.query.fName, lastName: request.query.lName}, function (err, userAdded) {
-    if(err) console.log("There's a problem with the database: ", err);
-    else if(userAdded) console.log("New user inserted in the database");
-  });
-  response.sendStatus(200);
-});
-
-// removes entries from users and populates it with default users
-app.get("/reset", function (request, response) {
-  // removes all entries from the collection
-  db.remove({}, { multi: true }, function (err) {
-    if(err) console.log("There's a problem with the database: ", err);
-    else console.log("Database cleared");
-  });
-  // default users inserted in the database
-  db.insert(users, function (err, usersAdded) {
-    if(err) console.log("There's a problem with the database: ", err);
-    else if(usersAdded) console.log("Default users inserted in the database");
-  });
-  response.redirect("/");
-});
-
-// removes all entries from the collection
-app.get("/clear", function (request, response) {
-  db.remove({}, { multi: true }, function (err) {
-    if(err) console.log("There's a problem with the database: ", err);
-    else console.log("Database cleared");
-  });
-  response.redirect("/");
+app.get('/*css', function(request, response) {
+  response.sendFile(__dirname + '/public/css' + request['url']);
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(54110, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+var WebHelper = new function() {
+
+  this.basepath = '';
+
+  this.init = function init(basepath) {
+    this.basepath = basepath;
+  };
+
+  /**
+   *
+   * @param {string} path
+   * @returns {undefined}
+   */
+  this.getUrl = function getUrl(path) {
+    if (path.charAt(0) === '/') {
+      return this.basepath + path;
+    } else {
+      return document.URL + '/' + path;
+    }
+  };
+};
+
+

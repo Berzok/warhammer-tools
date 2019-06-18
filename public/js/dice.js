e09172be1bdf9480a32db4755149f285cc8307d4
Berzok <34751752+Berzok@users.noreply.github.com>
2019-06-18 22:48:13 +0200

    Correction position div inventaire dans la page des dés

--- public/js/dice.js
+++ public/js/dice.js
@@ -9,5 +9,6 @@ $('.diceIcon').each(function(){
 function addDice(id){
     console.dir(id);
     var inventaire = $('#inventory');
+
     inventaire.append("<img class='imageIcon diceIcon' id='"+id+"' src='../img/"+id+".png' style='margin-bottom: 20px'>");
 }
